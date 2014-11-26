e9d22671fb7e51b5c3d3e24eb8d8773321b15c72
Conan <conan.cook@brightnorth.co.uk>
2014-11-26 15:06:56 +0000

    Parallel testing

--- project.clj
+++ project.clj
@@ -7,4 +7,8 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
-  :dependencies [[org.clojure/clojure "1.6.0"]])
+  :dependencies [[clj-webdriver "0.6.1"]
+                 [org.clojure/clojure "1.6.0"]
+                 [org.clojure/tools.logging "0.3.1"]
+                 [org.seleniumhq.selenium/selenium-java "2.44.0"]
+                 ])
--- src/examinant/core.clj
+++ src/examinant/core.clj
@@ -1,6 +1,36 @@
-(ns examinant.core)
+(ns examinant.core
+  (:import [org.openqa.selenium.remote DesiredCapabilities RemoteWebDriver])
+  (:require [clojure.java.io :refer [as-url]]
+            [clojure.test :refer [testing]]
+            [clojure.tools.logging :refer [debug info error]]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+
+(defn create-driver
+  "Creates a new RemoteWebDriver connected to the specified url, with the specified capabilities"
+  [url browser-spec]
+  (let [set-capability (fn [desired-capabilities browser-spec]
+                         (.setCapability desired-capabilities (name (first browser-spec)) (second browser-spec))
+                         desired-capabilities)
+        desired-capabilities (reduce set-capability (DesiredCapabilities.) browser-spec)]
+    (debug "Creating RemoteWebDriver with capabilities:" desired-capabilities)
+    (RemoteWebDriver. (as-url url) desired-capabilities)))
+
+
+(defn remote-tests
+  "Runs each of the specified tests once for each browser-spec, using the remote server at url"
+  [url browser-specs tests]
+  (try
+    (let [result-futures (for [test tests
+                               browser-spec browser-specs]
+                           (let [driver (create-driver url browser-spec)]
+                             (future (testing (str "Capabilities: " browser-spec)
+                                       (test driver)
+                                       (.quit driver)))))]
+      (doall (map #(try
+                    (deref %)
+                    (catch Throwable t
+                      (error t "Failed to complete remote test:"))) result-futures)))
+    (catch Throwable t
+      (error t "Error in examinant"))
+    (finally
+      (shutdown-agents))))
