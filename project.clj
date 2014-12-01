49b67d482ea6a4f178d20c5e277a38dd5db0d7e8
Conan <conan.cook@brightnorth.co.uk>
2014-12-01 14:15:23 +0000

    Caught exceptions logged as test failures

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject brightnorth/examinant "0.1.0"
+(defproject brightnorth/examinant "0.1.1-SNAPSHOT"
 
   :description "A library for running parallel remote WebDriver tests on remote infrastructure"
 
--- src/examinant/core.clj
+++ src/examinant/core.clj
@@ -2,18 +2,18 @@
   (:import [org.openqa.selenium.remote DesiredCapabilities RemoteWebDriver]
            [org.openqa.selenium.support.ui ExpectedCondition WebDriverWait])
   (:require [clojure.java.io :refer [as-url]]
-            [clojure.test :refer [testing]]
+            [clojure.test :refer [is testing]]
             [clojure.tools.logging :refer [debug info error]]))
 
 
 (defn create-driver
   "Creates a new RemoteWebDriver connected to the specified url, with the specified capabilities"
   [url browser-spec]
-  (let [set-capability (fn [desired-capabilities browser-spec]
-                         (.setCapability desired-capabilities (name (first browser-spec)) (second browser-spec))
-                         desired-capabilities)
+  (let [set-capability (fn [desired-caps browser-spec]
+                         (.setCapability desired-caps (name (first browser-spec)) (second browser-spec))
+                         desired-caps)
         desired-capabilities (reduce set-capability (DesiredCapabilities.) browser-spec)]
-    (debug "Creating RemoteWebDriver with capabilities:" desired-capabilities)
+    (debug "Creating RemoteWebDriver with capabilities:" (str desired-capabilities))
     ;; SauceLabs doesn't seem to have an SSL endpoint, so sending creds in the URL is a bit dubious
     (RemoteWebDriver. (as-url url) desired-capabilities)))
 
@@ -25,21 +25,30 @@
     (let [result-futures (for [test tests
                                browser-spec browser-specs]
                            (let [driver (create-driver url browser-spec)]
-                             (future (testing (str "Capabilities: " browser-spec)
-                                       (test driver)
-                                       (.quit driver)))))]
+                             (future
+                               (testing (str "Capabilities: " browser-spec)
+                                 (try
+                                   (test driver)
+                                   ;; Throwing an exception would cause clojure.test to abort all
+                                   ;; the other remote tests, so we wrap it in an assertion instead
+                                   (catch Throwable t
+                                     (is (nil? (format "test: %s Capabilities: %s Throwable: %s"
+                                                       test browser-spec (str t)))
+                                       "Examinant caught throwable in remote test")))
+                                 (.quit driver)))))]
       ;; TODO: handle when derefing a future throws an exception, and carry on derefing the others
       (doall (map deref result-futures)))
     (catch Throwable t
-      (error t "Error in examinant"))
+      (error t " Error in examinant "))
     (finally
       (shutdown-agents))))
 
 
 (defn wait-until
-  "Instructs the driver to wait until the predicate returns true or the timeout (in seconds) expires, in which case a
-  TimeoutException is thrown.  The predicate will be re-tried over and over, so should be idempotent.  If no timeout is
-  specified, a default is used.  The predicate must take a single argument, which is the WebDriver."
+  "Instructs the driver to wait until the predicate returns true or the timeout (in seconds) expires,
+  in which case a TimeoutException is thrown. The predicate will be re-tried over and over, so should
+  be idempotent. If no timeout is specified, a default is used. The predicate must take a single
+  argument, which is the WebDriver."
   ([driver predicate]
     (wait-until driver predicate 20))
   ([driver predicate timeout]
