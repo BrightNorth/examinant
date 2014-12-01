4cd0fb8237e6c869822ce6abd220932ec3fb6f82
Conan <conan.cook@brightnorth.co.uk>
2014-12-01 14:15:49 +0000

    0.1.1 release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject brightnorth/examinant "0.1.1-SNAPSHOT"
+(defproject brightnorth/examinant "0.1.1"
 
   :description "A library for running parallel remote WebDriver tests on remote infrastructure"
 
